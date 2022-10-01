147bfd64bbf88bb8380f8bf00b5f4c6946ec8a52
newmanga <newmanga.100@gmail.com>
2022-09-30 22:58:41 -0700

    Update image and links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import logo from './logo.svg';
+import logo from './kingGardenistLogo.png';
+import instaLogo from './InstagramLogo.png';
+import tiktokLogo from './TikTokLogo.png';
+import etsytLogo from './EtsyLogo.png';
 import './App.css';
 
 function App() {
@@ -7,16 +10,43 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Your favorite premium nature stickers!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+		<div>
+			<div className="SocialMedia">
+				<img src={etsytLogo} className="SocialMedia-logo" alt="etsytLogo" />
+				<a
+				  className="App-link"
+				  href="https://www.etsy.com/shop/KindGardenist?ref=profile_header"
+				  target="_blank"
+				  rel="noopener noreferrer"
+				>
+				Etsy
+				</a>
+			</div>
+			<div className="SocialMedia">
+				<img src={instaLogo} className="SocialMedia-logo" alt="instaLogo" />
+				<a
+				  className="App-link"
+				  href="https://www.instagram.com/kindgardenist/"
+				  target="_blank"
+				  rel="noopener noreferrer"
+				>
+				  Instagram
+				</a>
+			</div>
+			<div className="SocialMedia">
+				<img src={tiktokLogo} className="SocialMedia-logo" alt="tiktokLogo" />
+				<a
+				  className="App-link"
+				  href="https://www.tiktok.com/@kindgardenist?lang=en"
+				  target="_blank"
+				  rel="noopener noreferrer"
+				>
+				  TikTok
+				</a>
+			</div>
+		</div>
       </header>
     </div>
   );
