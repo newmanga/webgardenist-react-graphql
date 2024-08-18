7a5d5c21d8ff005da6d981523f5c33505da986b6
newmanga <newmanga.100@gmail.com>
2024-08-18 16:28:44 -0700

    Test to send email from lambda

--- amplify/backend/function/KGContactHandler/src/index.py
+++ amplify/backend/function/KGContactHandler/src/index.py
@@ -1,15 +1,48 @@
 import json
+import boto3
+
+def send_email(subject, body, sender, recipient):
+    client = boto3.client('ses', region_name='us-east-2')
+    response = client.send_email(
+        Source=sender,
+        Destination={
+            'ToAddresses': [recipient]
+        },
+        Message={
+            'Subject': {
+                'Data': subject
+            },
+            'Body': {
+                'Text': {
+                    'Data': body
+                }
+            }
+        }
+    )
+    return response['MessageId']
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
+    print('received event:')
+    print(event)
+
+    statusCode = 200
+    email_result = {}
+    subject = "Hello from AWS Lambda"
+    body = "This is the body of the email."
+    sender = "kindgardenist@gmail.com"
+    recipient = "kindgardenist@gmail.com"
+
+    try:
+        email_result = send_email(subject, body, sender, recipient)
+    except:
+        statusCode = 400
+    
+    return {
+        'statusCode': statusCode,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': email_result
   }
